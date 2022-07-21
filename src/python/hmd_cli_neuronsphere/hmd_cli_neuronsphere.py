749fb62e591fe9000da85b09c1257da1d2a99e50
Adam Stortz <adam.stortz@hmdlabs.io>
2022-07-21 15:09:13 -0500

    feat: adds dag_generators mount and new trino conn

--- src/python/hmd_cli_neuronsphere/hmd_cli_neuronsphere.py
+++ src/python/hmd_cli_neuronsphere/hmd_cli_neuronsphere.py
@@ -165,6 +165,7 @@ def start_neuronsphere():
     if configs.get("transform").get("enabled"):
         required_dirs += [Path("transform", "airflow", "logs")]
         required_dirs += [Path("transform", "airflow", "provider_transforms")]
+        required_dirs += [Path("transform", "airflow", "dag_generators")]
         required_dirs += [Path("data", "local_transforms")]
     for dir in required_dirs:
         full_dir = _hmd_home / dir
