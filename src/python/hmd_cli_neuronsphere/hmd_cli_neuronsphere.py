81754b37aa38b603261e091249e5da9060b73afa
Adam Stortz <adam.stortz@hmdlabs.io>
2022-08-18 10:57:28 -0500

    feat: adds support for disabling local project service

--- src/python/hmd_cli_neuronsphere/hmd_cli_neuronsphere.py
+++ src/python/hmd_cli_neuronsphere/hmd_cli_neuronsphere.py
@@ -33,8 +33,12 @@ def load_env():
     load_dotenv(_hmd_home / ".config" / "hmd.env", override=True)
 
 
-def _get_tech_enabled(name):
-    return _get_env_var(f"HMD_LOCAL_NEURONSPHERE_ENABLE_{name}") == "true"
+def _get_tech_enabled(name, default=None):
+    var = f"HMD_LOCAL_NEURONSPHERE_ENABLE_{name}"
+    val = _get_env_var(var)
+    if val is not None:
+        return val == "true"
+    return default
 
 
 def _get_configs():
@@ -44,6 +48,7 @@ def _get_configs():
             if _get_env_var("HMD_REPO_HOME") is not None
             else None
         )
+        enable_project = _get_tech_enabled("PROJECT", True)
         enable_trino = _get_tech_enabled("TRINO")
         enable_dynamodb = _get_tech_enabled("DYNAMODB")
         enable_hmd_ms_core = _get_tech_enabled("HMD_MS_CORE")
@@ -58,6 +63,10 @@ def _get_configs():
                 "base": {
                     "enabled": True,
                     "path": _services_dir / f"docker-compose.main.yml",
+                },
+                "project": {
+                    "enabled": enable_project,
+                    "path": _services_dir / f"docker-compose.project.yml",
                 },
                 "jupyter": {
                     "enabled": True,
