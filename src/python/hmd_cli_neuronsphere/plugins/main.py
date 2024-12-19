f622bcad0db393a0c1ff608486ef47976686aba5
Alex Burgoon <alex.burgoon@hmdlabs.io>
2024-12-19 15:07:07 -0500

    feat: adds gozer

--- src/python/hmd_cli_neuronsphere/plugins/main.py
+++ src/python/hmd_cli_neuronsphere/plugins/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pathlib import Path
 import shutil
@@ -75,6 +76,48 @@ def render_compose_yaml(
     compose_dict = {}
     with open(_services_dir / "docker-compose.main.yml", "r") as dc:
         compose_dict = yaml.safe_load(dc)
+
+    gozer_config = {
+        "container_name": "ms-gozer",
+        "image": f"{os.environ.get('HMD_LOCAL_NS_CONTAINER_REGISTRY', 'ghcr.io/neuronsphere')}/hmd-ms-gozer:{os.environ.get('HMD_MS_GOZER_VERSION', 'stable')}",
+        "environment": {
+            "HMD_INSTANCE_NAME": "ms-gozer",
+            "HMD_REPO_NAME": "hmd-ms-gozer",
+            "HMD_REPO_VERSION": "0.1.45",
+            "HMD_CUSTOMER_CODE": "${HMD_CUSTOMER_CODE}",
+            "HMD_DID": "${HMD_DID:-aaa}",
+            "HMD_ENVIRONMENT": "local",
+            "HMD_REGION": "${HMD_REGION}",
+            "RDS_SECRETS": json.dumps(
+                {
+                    db["database"]: {**db, "host": "db", "port": "5432"}
+                    for db in resources["databases"]
+                }
+            ),
+            "NEPTUNE_ENDPOINTS": json.dumps({"global-graph": "global-graph"}),
+            "LIBRARIAN_DYNAMO_TABLES": "{}",
+            "S3_BUCKETS": "{}",
+            "DYNAMO_TABLE_NAMES": "[]",
+            "AWS_DEFAULT_REGION": "${AWS_REGION:-us-west-2}",
+            "AWS_ACCESS_KEY_ID": "dummykey",
+            "AWS_SECRET_ACCESS_KEY": "dummykey",
+            "AWS_XRAY_SDK_ENABLED": "false",
+            "DD_API_KEY": "${DD_API_KEY:-dummykey}",
+            "DD_APP_KEY": "${DD_APP_KEY:-dummykey}",
+            "DD_FLUSH_TO_LOG": "true",
+            "DD_SERVERLESS_LOGS_ENABLED": "false",
+            "DD_LAMBDA_HANDLER": "hmd_ms_base.hmd_ms_base.handler",
+            "DD_TRACE_ENABLED": "false",
+            "DD_LOCAL_TEST": "true",
+            "DD_LOG_LEVEL": "debug",
+            "SERVICE_CONFIG": '{"operations_modules": ["hmd_ms_gozer.hmd_ms_gozer"]}',
+        },
+        "depends_on": {"db": {"condition": "service_healthy"}},
+        "networks": ["neuronsphere_default"],
+    }
+
+    if os.environ.get("ENABLE_GOZER", "false") == "true":
+        compose_dict["services"]["ms-gozer"] = gozer_config
 
     compose_path = cache_dir / "docker-compose.main.yml"
     if compose_path.exists():
